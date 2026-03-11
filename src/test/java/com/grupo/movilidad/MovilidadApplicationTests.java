a9f33330ad6dcc25567bf3b8354bf735bb49a46c
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2026-03-11 20:52:40 -0300

    Primer commit, inicio de proyecto Movilidad

---
+++ src/test/java/com/grupo/movilidad/MovilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.movilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
